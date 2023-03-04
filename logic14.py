fe47f7a64d920c344ff7cfb0f30847b03b86934d
husan999 <husanshunqorov07@gmail.com>
2023-03-04 11:14:19 +0500

    good

--- logic14.py
+++ logic14.py
@@ -6,5 +6,5 @@ def main(a):
     Returns:
         bool: answer
     """
-    return a==int(a) and a>=10 and a<=99 and (a//10+a%10)>=1
+    return a==int(a) and a>=10 and a<=99 and (a//10+a%10)%2!=0
 print(main(23))
